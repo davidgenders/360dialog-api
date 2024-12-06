3821114148e892a4b73d0944c545365edf279edc
davidgenders <dagenders@gmail.com>
2024-12-06 10:52:23 +0200

    lib for npm install

---
+++ lib/types/utils.d.ts
@@ -0,0 +1,10 @@
+export type AtLeastOne<T> = [T, ...T[]];
+export type MaybePromise<T> = T | Promise<T> | PromiseLike<T>;
+export declare function isInteger(n: unknown): n is number;
+export declare function escapeUnicode(str: string): string;
+type Without<T, U> = {
+    [P in Exclude<keyof T, keyof U>]?: never;
+};
+export type XOR<T, U> = T | U extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U;
+export {};
+//# sourceMappingURL=utils.d.ts.map
